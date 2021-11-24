7c7ee69df103b18961e2b220f737f5393b1aae5f
Sergio Mercado Monetti <sergioimone@outlook.com>
2021-11-24 16:03:25 -0600

    added travis

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 docker build -t sergiomermont/multi-client:latest -t sergiomermont/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t sergiomermont/multi-server:latest -t sergiomermont/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t sergiomermont/multi-worker:latest -t sergiomermont/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push sergiomermont/multi-client
-docker push sergiomermont/multi-server
-docker push sergiomermont/multi-worker
+docker push sergiomermont/multi-client:latest
+docker push sergiomermont/multi-client:$SHA
+docker push sergiomermont/multi-server:latest
+docker push sergiomermont/multi-server:$SHA
+docker push sergiomermont/multi-worker:latest
+docker push sergiomermont/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=sergiomermont/multi-server
+kubectl set image deployments/server-deployment server=sergiomermont/multi-server:$SHA
+kubectl set image deployments/client-deployment client=sergiomermont/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=sergiomermont/multi-worker:$SHA
